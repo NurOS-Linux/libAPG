d2ea8a5d2b1b3911272db7838ed4d1b9711d89c1
ruzen42 <malledeferlive@gmail.com>
2026-01-12 17:34:28 +0500

    fix(config.h)

--- include/apg/config.h
+++ include/apg/config.h
@@ -1,13 +1,30 @@
-// NurOS Ruzen42 2025 apg/archive.h
-// Last change: Jan 11
+// NurOS Ruzen42 2026 apg/config.h
+// Last change: Jan 12
 #ifndef APG_CONFIG_H
 #define APG_CONFIG_H 
 
+
+typedef enum {
+  HTTP,
+  FTP,
+  RSYNC,
+} repo_type;
+
+typedef struct {
+  const char *url;
+  repo_type type;
+} repo;
+
+// all config in /etc/apg.conf
 typedef struct {
   int db_size;
+  char *tmp_dir;
+  size_t repo_count;
+  repo *repos;
 } config;
 
-config parse_config(char *);
+config *parse_config(char *);
 void set_config(config *);
+void config_free(config *);
 
 #endif //APG_CONFIG_H
