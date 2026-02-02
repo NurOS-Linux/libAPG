e8a8f5c97741b8fdfaed10d9af71294c1b0aeeae
ruzen42 <malledeferlive@gmail.com>
2026-02-02 21:08:11 +0500

    fix

--- include/apg/db.h
+++ include/apg/db.h
@@ -1,15 +1,18 @@
-// NurOS Ruzen42 2025 apg/db.h
-// Last change: Jan 11 
+// NurOS Ruzen42 2026 apg/db.h
+// Last change: Jan 15 
 
 #ifndef APG_DB_H
 #define APG_DB_H
 
-#include "package.h"
+#include <lmdb.h>
 #include <stdbool.h>
 
-bool add_package(struct package *pkg, char *db_path);
-bool remove_package(char *pkg_name, char *db_path);
-struct package *get_all_packages(char *db_path);
-struct package get_package_by_name(char *name, char *db_path);
+#include "package.h"
+
+MDB_env *init_db(char *db_path);
+bool add_package(struct package *pkg, MDB_env *env);
+bool remove_package(char *pkg_name, MDB_env *env);
+struct package *get_all_packages(MDB_env *env);
+struct package get_package_by_name(char *name, MDB_env *env);
 
 #endif //APG_DB_H
--- src/db.c
+++ src/db.c
@@ -1,5 +1,5 @@
 // NurOS Ruzen42 2026 apg/db.c
-// Last change: Jan 11
+// Last change: Jan 15
 
 #include <lmdb.h>
 #include <stdbool.h>
@@ -7,25 +7,40 @@
 #include "../include/apg/db.h"
 #include "../include/apg/package.h"
 
+
+MDB_env * 
+init(
 bool 
-add_package(struct package *pkg, char *db_path)
+add_package(struct package *pkg, MDB_env *env)
 {
-    
+  MDB_dbi dbi;
+  MDB_txn *txn;
+  MDB_val key, data;
+
+  if (mdb_txn_begin(env, NULL, 0, &txn)) 
+    return false;
+
+  if (mdb_dbi_open(txn, NULL, MDB_CREATE, &dbi)) {
+    mdb_txn_abort(txn);
+    return false;
+  }
+
+
 }
 
 bool 
-remove_package(char *pkg_name, char *db_path)
+remove_package(char *pkg_name, MDB_env *env)
 {
 
 }
 
 struct package *
-get_all_packages(char *db_path)
+get_all_packages(MDB_env *env)
 {
 }
 
 struct package 
-get_package_by_name(char *name, char *db_path)
+get_package_by_name(char *name, MDB_env *env)
 {
 }
 
