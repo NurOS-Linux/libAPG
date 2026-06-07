52891a0334e88962facebc6e67e5c8c0a07d81ad
AnmiTaliDev <anmitali198@gmail.com>
2026-06-07 15:33:28 +0500

    docs: add Doxygen comments to copy.h

--- include/apg/copy.h
+++ include/apg/copy.h
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // SPDX-FileCopyrightText: 2026 AnmiTaliDev <anmitalidev@nuros.org>
+
 #pragma once
+
+/**
+ * @file copy.h
+ * @brief Recursive directory copy utility.
+ */
 
 #include <stdbool.h>
 
+/**
+ * @brief Recursively copy a directory tree.
+ *
+ * Creates @p dst if it does not exist. Existing files in @p dst are
+ * overwritten. Permissions and timestamps are preserved.
+ *
+ * @param src Source directory path.
+ * @param dst Destination directory path.
+ * @return true on success, false if any file or directory could not be copied.
+ */
 bool copy_dir(const char *src, const char *dst);
