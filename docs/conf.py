63ad2b6b46b1c497db7517a3698b45cfd1a30476
AnmiTaliDev <anmitali198@gmail.com>
2026-06-07 15:32:21 +0500

    docs: add Doxygen + Sphinx documentation pipeline

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = "libapg"
+author = "AnmiTaliDev"
+release = "1.4.0"
+copyright = "2026, AnmiTaliDev"
+
+extensions = [
+    "breathe",
+    "sphinx.ext.intersphinx",
+]
+
+breathe_projects = {"libapg": "../docs/_doxygen/xml"}
+breathe_default_project = "libapg"
+breathe_domain_by_extension = {"h": "c"}
+
+breathe_default_members = ("members", "undoc-members")
+
+html_theme = "furo"
+html_title = "libapg 1.4.0"
+
+intersphinx_mapping = {}
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+libapg
+======
+
+Package management library for NurOS.
+
+libapg provides the core building blocks for package managers: dependency
+resolution, an LMDB-backed package database, atomic install/remove
+transactions, checksum verification, package signing, and a persistent
+operation journal.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+
+   api/index
